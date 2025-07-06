c6cf729a8ce9df6844ef3f31fbf83585040c95f0
Dao Khai <khaidaofpt@gmail.com>
2025-07-06 14:01:35 +0700

    Push base project DATN

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-orange-500">Hello Khải!</h1>
+      <p className="text-gray-500">Tailwind chạy ngon lành rồi 😎</p>
+    </div>
+  );
+}
+
+export default App
