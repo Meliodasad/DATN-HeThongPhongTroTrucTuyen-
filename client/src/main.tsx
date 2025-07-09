07d5af4c11d557294897846345a3eaee263c95b9
buixuantung1811 <tungbxph51290@gmail.com>
2025-07-09 12:45:00 +0700

    Code của Xuân Tùng

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'  // <- Thêm dòng này
 import './index.css'
 import App from './App.tsx'
-
+import 'antd/dist/reset.css'; // Ant Design 5+
+import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>  {/* <- Bọc App bằng BrowserRouter */}
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
