a77b268333d69c0a2d1c3911518ce8f5d21f9b05
quangan0512 <annqph51610@gmail.com>
2025-07-28 15:27:09 +0700

    sua code moi

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,13 +4,16 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext.tsx'
+import { ToastProvider } from './contexts/ToastContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <StrictMode>
-      <AuthProvider>
+       <ToastProvider>
+        <AuthProvider>
          <App />
       </AuthProvider>
+       </ToastProvider>
     </StrictMode>
   </BrowserRouter>
 )
